8491266d718c4f8e309ec1f3c95c309363d5db65
ibrahim0015 <muhammadibrahim15th@gmail.com>
2025-11-13 17:22:38 +0500

    commiting a new file.

---
+++ butterfly pattern.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a number: ";
+    cin>>n;
+    int space1= n;
+    int space2=0;
+    // upper part.
+    for (int i=0; i<n; i++){
+    for(int j=0; j<=i;j++){
+        cout<<"*";    
+    }
+    for(int k=0;k<2*space1-2;k++){
+        cout<<" ";
+    }    
+    for(int j=0;j<=i;j++){
+        cout<<"*";
+    }
+        space1=space1-1;
+        cout<<endl;
+}
+    
+    // lower part.
+    for(int i=0;i<n;i++){
+        for(int j=n;j>i;j--){
+            cout<<"*";
+        }
+        for(int k=0;k<2*space2;k++){
+            cout<<" ";
+        }
+    for(int j=n;j>i;j--){
+            cout<<"*";
+        }
+
+        space2++;
+        cout<<endl;
+    }
+    
+    
+    
+    
+}
